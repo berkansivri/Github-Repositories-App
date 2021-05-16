0859cc15a7d48c6f730b8239017947e6fc031358
berkansivri <berkansivri@gmail.com>
2021-05-16 21:29:23 +0300

    comment

--- src/boot/mixin.js
+++ src/boot/mixin.js
@@ -1,7 +1,7 @@
 export default {
   methods: {
     normalizeUrl: function(url) {
-      return url.split("{")[0];
+      return url.split("{")[0]; // removes /{sha} part from the url's of GitHub API
     },
     sortByContentType(content) {
       return content.type === "file" ? 1 : -1;
